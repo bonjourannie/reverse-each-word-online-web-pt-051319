912f67ad320223c378bbb2808cfbc446f23dd4f0
Annie Truex <artruex@gmail.com>
2019-05-17 03:41:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
-sentence = ["Hello", "there", "and", "how", "are", "you", "?"]
-
-
-def reverse_each_word(words)
-  words.each reverse |word|
+def reverse_each_word(string)
+original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
 end
