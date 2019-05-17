eb5ec24ca2742301de5a25936f4ae979c741166b
Annie Truex <artruex@gmail.com>
2019-05-17 03:45:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,11 @@ original_array = string.split(" ")
   return_array.join(" ")
 end
 
+def reverse_each_word(string)
+  array = string.split(" ")
+  test_array = []
+  array.collect do|string| 
+    test_array << string.reverse 
+  end
+  test_array.join(" ")
+end
